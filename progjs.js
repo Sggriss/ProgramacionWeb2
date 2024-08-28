8dd3e09fcf76f9c6d8d1cd51558076f354c5ac17
Mairan Grisselle Silva Garces <98857407+Sggriss@users.noreply.github.com>
2024-08-27 22:40:31 -0600

    Add files via upload

---
+++ progjs.js
@@ -0,0 +1,14 @@
+let nombreusuario = prompt ("Ingrese su nombre: ")
+
+let edad = prompt ("Ingrese su edad: ")
+
+let Estatura = prompt("Ingrese su estatura: ")
+
+let peso = prompt("Ingrese su peso: ")
+
+
+let estatura2 = Estatura * Estatura
+console.log("resultado de la multiplicacion de la estatura "+ estatura2)
+let imc = peso / estatura2 
+
+console.log("Este es el imc " + imc)
